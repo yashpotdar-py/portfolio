042be26fc45e6d4e6132e941e0dd81b00dffe1f1
SynTech <150009480+syntech-97@users.noreply.github.com>
2023-11-10 12:45:56 +0530

    Adding the sidebar using the layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,12 @@
-import './index.scss';
+import Sidebar from '../Sidebar'
+import './index.scss'
 
 const Layout = () => {
-    return <>Hello</>
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
 }
 
 export default Layout;
