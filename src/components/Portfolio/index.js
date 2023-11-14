4993b51717fd25661b04282a9135196d19d7cfeb
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2023-11-14 19:28:32 +0530

    Completing the Portfolio Page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,12 +6,40 @@ import portfolioData from '../../data/portfolio.json'
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  console.log(portfolioData)
+
   useEffect(() => {
     return setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.cover}
+                alt={portfolio}
+                className="portfolio-image"
+              />
+              <div className="portfolio-content">
+                <p className="portfolio-title">{port.title}</p>
+                <h4 className="portfolio-description">{port.description}</h4>
+                <button
+                  className="portfolio-btn"
+                  onClick={() => window.open(port.url)}
+                >
+                  VIEW
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 
   return (
     <>
@@ -24,8 +52,10 @@ const Portfolio = () => {
               idx={15}
             />
           </h1>
+          <div>{renderPortfolio(portfolioData.portfolio)}</div>
         </div>
       </div>
+
       <Loader type="line-scale" />
     </>
   )
