e3bb9b6681045296175f6c3388a7764ec835f8e9
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2023-11-17 21:32:53 +0530

    Updating the content of About Me and Skills

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 import { useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faGitAlt,
@@ -24,70 +25,90 @@ const About = () => {
 
   return (
     <>
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={"About Me".split("")}
-            idx={15}
-          />
-        </h1>
-        <p>
-          Hello there! I'm Yash Potdar, a second-year B. Tech student immersing
-          myself in the fascinating world of Artificial Intelligence and Data
-          Science. My passion lies in cybersecurity, where I channel my
-          dedication to transforming theoretical knowledge into practical
-          applications. I'm committed to seamlessly integrating concepts into
-          real-world solutions, and I'm thrilled to share my journey with you.
-        </p>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={'About Me'.split('')}
+              idx={15}
+            />
+          </h1>
+          <p>
+            <strong>Introduction:</strong> Welcome to my corner of the digital
+            world! I'm Yash Potdar, a second-year B. Tech student with a
+            profound passion for technology and a keen interest in Artificial
+            Intelligence and Data Science.
+          </p>
 
-        <p>
-          Currently navigating the intricacies of AI and Data Science, I am
-          fueled by a curiosity to unravel the complexities of data and leverage
-          its power for innovation. My coursework lays a solid foundation, and
-          I'm eager to delve deeper into machine learning, deep learning, and
-          data analytics. The pursuit of knowledge in these areas drives me, and
-          I'm excited about the possibilities they hold for the future.
-        </p>
+          <p>
+            <strong>Education:</strong> Currently, I am pursuing my Bachelor's
+            in Technology with a major in Artificial Intelligence and Data
+            Science at AISSMS IOIT. My coursework not only provides a strong
+            theoretical foundation but also sparks my curiosity to explore the
+            intricacies of AI, machine learning, deep learning, and data
+            analytics.
+          </p>
 
-        <p>
-          As a tech enthusiast with certificates in web development, Python, and
-          IT automation, I go beyond being just a student. I find joy in
-          problem-solving through coding and automation, always seeking new
-          challenges. Beyond the code, my interest extends into the dynamic
-          realm of cybersecurity. The ever-evolving landscape of digital threats
-          fuels my passion to understand, combat, and contribute to securing the
-          digital world. I firmly believe that a secure cyberspace is the
-          foundation of technological progress, and I am dedicated to staying at
-          the forefront of this vital field.
-        </p>
-      </div>
+          <p>
+            <strong>Tech Enthusiast:</strong> As a tech enthusiast, I find joy
+            in coding and problem-solving. I hold certificates in web
+            development, Python, and IT automation, showcasing my commitment to
+            mastering various facets of the digital realm. I thrive on tackling
+            new challenges, constantly seeking opportunities to expand my skill
+            set and explore innovative solutions.
+          </p>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faPython} color="#3775A8" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faGithub} color="#000" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faLinux} color="#000" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          <p>
+            <strong>Cybersecurity Passion:</strong> Beyond the code, my passion
+            extends into the dynamic domain of cybersecurity. I am dedicated to
+            understanding, combatting, and contributing to securing the digital
+            world. I firmly believe that a secure cyberspace is the bedrock of
+            technological progress, and I am committed to staying at the
+            forefront of this vital field.
+          </p>
+
+          <p>
+            Join me on this journey of exploration and growth as I navigate the
+            ever-evolving landscape of technology. Let's connect, collaborate,
+            and learn together!
+            <br />
+            <NavLink
+              exact="true"
+              activaclassname="active"
+              className="aboutme-link"
+              target="_blank"
+              to="https://www.linkedin.com/in/yashyogeshpotdar/"
+            >
+              View my LinkedIn Profile
+            </NavLink>
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faPython} color="#3775A8" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faGithub} color="#000" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faLinux} color="#000" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Loader type='line-scale' />    
+      <Loader type="line-scale" />
     </>
   )
 }
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 import Loader from 'react-loaders'
+import { NavLink } from 'react-router-dom'
 
 const Skills = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -18,10 +19,66 @@ const Skills = () => {
           <h1>
             <AnimatedLetters
               letterClass={letterClass}
-              strArray={"Skills & Experience".split("")}
+              strArray={'Skills & Experience'.split('')}
               idx={15}
             />
           </h1>
+          <p>
+            <strong>Technical Skills:</strong> Proficient in Python with a focus
+            on building robust and efficient applications. Skilled in HTML, CSS,
+            and EJS for creating dynamic and responsive web applications. Solid
+            understanding of AI, machine learning, and data analytics, with
+            hands-on experience in projects such as{' '}
+            <NavLink
+              exact="true"
+              activaclassname="active"
+              className="project-link"
+              to="/portfolio"
+            >
+              Flappy Bird AI
+            </NavLink>
+            &nbsp;and&nbsp;
+            <NavLink
+              exact="true"
+              activaclassname="active"
+              className="project-link"
+              to="/portfolio"
+            >
+              Credit Card Fraud Detection
+            </NavLink>
+            .
+          </p>
+
+          <p>
+            <strong>Certifications:</strong> Certified in various Google
+            courses, including Crash Course on Python, Using Python to Interact
+            with OS, and Troubleshooting and Debugging Techniques on Coursera.
+            Proficient in version control using Git and experienced in
+            collaborative coding through GitHub.
+          </p>
+
+          <p>
+            <strong>Experience:</strong> Currently serving as a Technical
+            Executive in the Artificial Intelligence Student Association, where
+            I've played a key role in organizing technical events and providing
+            support for non-technical activities.
+          </p>
+
+          <p>
+            <strong>Tech Stack:</strong> Expertise in HTML, CSS, and EJS for
+            building visually appealing and user-friendly interfaces. Skilled in
+            Python, JavaScript, and NodeJS for developing robust backend
+            systems. Familiar with Linux Kernels, Ubuntu, and adept at
+            configuring and managing cloud resources.
+          </p>
+
+          <p>
+            <strong>Future Aspirations:</strong> My journey is an ongoing
+            commitment to continuous learning, exploration, and application of
+            cutting-edge technologies. I look forward to taking on new
+            challenges and contributing to the ever-evolving world of
+            technology.
+          </p>
         </div>
       </div>
       <Loader type="line-scale" />
